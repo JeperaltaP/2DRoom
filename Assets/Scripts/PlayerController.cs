ed8571004b85d70840a88036e3002529f02e281f
JeperaltaP <jeperaltap@eafit.edu.co>
2025-09-23 19:38:27 -0500

    text score UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using TMPro; //libreria para usar textos
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,11 +8,15 @@ public class PlayerController : MonoBehaviour
     public int score = 0;
     public bool hasKey = false;
     public bool hasWater = false;
+    public TextMeshProUGUI textScore;
+    public TextMeshProUGUI notificationsText;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UpdateTextScore();
+
+       notificationsText.text = "Collect 7 items to win!";
     }
 
     // Update is called once per frame
@@ -34,6 +39,7 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("Collectable"))
         {
             score = score + 1; //igual a: score+=1 ; score ++;
+            UpdateTextScore();
 
             Destroy(other.gameObject);
             Debug.Log("Collected!!!");
@@ -45,12 +51,14 @@ public class PlayerController : MonoBehaviour
             hasKey = true;
             Debug.Log("Key Collected!");
             Destroy(other.gameObject);
+            notificationsText.text = "Score: " + score + "/7";
         }
 
         //condición de victoria
         if (score >= 3 && hasKey && !hasWater) // el && significa "y", el || significa "o"; como en lógica: V||V = V, V||F = V, F||F = F
         {
             Debug.Log("Has ganado, no has tocado el agua, tienes suficientes puntos y la llave!");
+            notificationsText.text = "You have the key and won waos!!!";
         }
         // para booleanos, si se pone el nombre de la variable sola, se asume si es verdadero, si tiene "!" delante, se asume si es falso
         
@@ -59,6 +67,13 @@ public class PlayerController : MonoBehaviour
             hasWater = true;
             Debug.Log("Muelto");
             Destroy(gameObject);
+            notificationsText.text = "Muelto";
         }
     }
+
+    void UpdateTextScore()
+    {
+        textScore.text = "Score: " + score;
+    }
+
 }
