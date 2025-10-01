5f03e22890df24a26a48796450d487bbd0ac88bc
JJuandi14 <jdalferezs@eafit.edu.co>
2025-09-30 19:38:13 -0500

    Notifications,NewScene,Buttons

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,14 +9,12 @@ public class PlayerController : MonoBehaviour
     public bool hasKey = false;
     public bool hasWater = false;
     public TextMeshProUGUI textScore;
-    public TextMeshProUGUI notificationsText;
-    
+    public TextMeshProUGUI notificationText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         UpdateTextScore();
-
-       notificationsText.text = "Collect 7 items to win!";
     }
 
     // Update is called once per frame
@@ -27,7 +25,7 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         //creamos un vector para direccion del movimiento
-        Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0);  
+        Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0);
 
         transform.Translate(direction * speed * Time.deltaTime);
 
@@ -36,44 +34,49 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Collectable"))
+        if (other.CompareTag("Collectable"))
         {
             score = score + 1; //igual a: score+=1 ; score ++;
             UpdateTextScore();
 
             Destroy(other.gameObject);
             Debug.Log("Collected!!!");
+            ShowNotification("Collected!");
             Debug.Log("Score: " + score);
         }
-        
+
         if (other.CompareTag("Key"))
         {
             hasKey = true;
             Debug.Log("Key Collected!");
             Destroy(other.gameObject);
-            notificationsText.text = "Score: " + score + "/7";
         }
 
         //condición de victoria
         if (score >= 3 && hasKey && !hasWater) // el && significa "y", el || significa "o"; como en lógica: V||V = V, V||F = V, F||F = F
         {
             Debug.Log("Has ganado, no has tocado el agua, tienes suficientes puntos y la llave!");
-            notificationsText.text = "You have the key and won waos!!!";
+            ShowNotification("You have the key!!!");
         }
         // para booleanos, si se pone el nombre de la variable sola, se asume si es verdadero, si tiene "!" delante, se asume si es falso
-        
+
         if (other.CompareTag("Water"))
         {
             hasWater = true;
             Debug.Log("Muelto");
             Destroy(gameObject);
-            notificationsText.text = "Muelto";
+            ShowNotification("Muelto");
         }
     }
 
     void UpdateTextScore()
     {
         textScore.text = "Score: " + score;
+    }
+
+    void ShowNotification(string message)
+    {
+        notificationText.text = message;
     }
 
 }
