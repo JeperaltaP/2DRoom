3944627ef4467da134929afadfd9ed719690e6cf
JeperaltaP <jeperaltap@eafit.edu.co>
2025-10-14 19:29:43 -0500

    Menu UI, credits, GoMenu

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,10 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
 
 public class Buttons : MonoBehaviour
-{
+{   
+    public GameObject CreditsPanel;
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CreditsOn()
+    {
+        CreditsPanel.SetActive(true);
+    }
+
+    public void CreditsOff()
+    {
+        CreditsPanel.SetActive(false);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
